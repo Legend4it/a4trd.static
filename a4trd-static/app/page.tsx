51d413071d13ad8318c743a66f99357c5c0da1e8
Ali Abdulhussein <abdulhussein.ali@gmail.com>
2026-05-01 01:30:55 +0200

    initial commit

---
+++ a4trd-static/app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <h1>🚧 Coming Soon</h1>
+
+        <p>
+          This site is currently under construction.<br />
+          We’re working hard to launch something awesome!<br /><br />
+          Contact us:{" "}
+          <a href="mailto:info@a4trd.se">info@a4trd.se</a>
+        </p>
+
+        <div className="badge">Stay tuned</div>
+      </div>
+
+      <footer>
+        © 2026 Your Name
+      </footer>
+    </>
+  );
+}
